7bf45785b75ac43eaf179c6f7b6035ef3f59a40a
davyzhang3 <davyzhang325@gmail.com>
2022-07-23 08:42:28 -0400

    update the terraform module lab

---
+++ terraform module lab/modules/subnet/outputs.tf
@@ -0,0 +1,7 @@
+output "subnet"{
+    value = aws_subnet.terraform-ec2-subnet-1
+}
+
+output "route_table"{
+    value = aws_route_table.terraform-ec2-route-table
+}
---
+++ terraform module lab/modules/subnet/variables.tf
@@ -0,0 +1,4 @@
+variable vpc_id {}
+variable subnet_cidr_block {}
+variable avail_zone {}
+variable env_prefix {}
